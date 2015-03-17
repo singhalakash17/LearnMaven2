2197cffae4cc1ed40651cb3d053b4d2a0f19945c
Akash <akashs@exzeo.com>
2015-03-17 15:40:36 +0530

    a

---
+++ src/test/java/exzeo/Components/LearnMaven2/NewTest.java
@@ -0,0 +1,18 @@
+package exzeo.Components.LearnMaven2;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+	
+  @Test
+  public void f1() {
+
+	  System.out.println("FirstMethod of Second Test Case");
+  }
+  
+  @Test
+  public void f2() {
+
+	  System.out.println("SecondMethod of Second Test Case");
+  }
+}
---
+++ src/test/java/exzeo/Components/LearnMaven2/Test01.java
@@ -0,0 +1,13 @@
+package exzeo.Components.LearnMaven2;
+
+import org.testng.annotations.Test;
+
+public class Test01 {
+	
+	@Test
+	public void firstTest() {
+		
+		System.out.println("This is my first maven program");
+		
+	}
+}
